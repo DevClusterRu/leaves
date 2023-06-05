b64740b8b6b60d3e9006337ea1144e43a51efb24
Den <you@example.com>
2023-06-05 20:03:33 +0300

    Last

--- leaves_test.go
+++ leaves_test.go
@@ -6,9 +6,9 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/dmitryikh/leaves/mat"
-	"github.com/dmitryikh/leaves/transformation"
-	"github.com/dmitryikh/leaves/util"
+	"github.com/DevClusterRu/leaves/mat"
+	"github.com/DevClusterRu/leaves/transformation"
+	"github.com/DevClusterRu/leaves/util"
 )
 
 func isFileExists(filename string) bool {
